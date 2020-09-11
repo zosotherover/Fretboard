14ed7ecaad6d8544d423dd20628c86ce03015344
Bryan Pero <bryanpero@gmail.com>
2020-09-11 17:18:37 -0400

    working on landing page & nav bar components

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>The Fretboard</h1>
+          <p className='lead'>
+            Create a user profile, share posts, and talk shop with other guitar
+            players
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
