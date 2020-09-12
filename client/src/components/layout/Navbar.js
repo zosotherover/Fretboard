dfdbacd3dbfa7e8f4dfb4a78becc4a4a0ed2e6f3
Bryan Pero <bryanpero@gmail.com>
2020-09-12 12:11:44 -0400

    worked on register & login components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
-          <i className='fas fa-code'></i> The Fretboard
-        </a>
+        <Link to='/'>
+          <i className='fas fa-guitar'></i> The Fretboard
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='profiles.html'>Members</a>
+          <a href='!#'>Members</a>
         </li>
         <li>
-          <a href='register.html'>Register</a>
+          <Link to='/register'>Register</Link>
         </li>
         <li>
-          <a href='login.html'>Login</a>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </nav>
