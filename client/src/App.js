1a253712f3f03a0c8013875ff19efdd7d5b35bf2
Bryan Pero <bryanpero@gmail.com>
2020-09-14 03:18:18 -0400

    CommentForm.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,9 @@ import EditProfile from './components/profile-forms/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
+
 // Redux imports:
 import { Provider } from 'react-redux';
 import store from './store';
@@ -54,6 +56,7 @@ const App = () => {
                 component={EditProfile}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
