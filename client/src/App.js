4b927c4e1614078aa3d65cd0cc4821eb9bfe003d
Bryan Pero <bryanpero@gmail.com>
2020-09-14 02:51:25 -0400

    working on posts/post likes functionality

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux imports:
 import { Provider } from 'react-redux';
@@ -52,6 +53,7 @@ const App = () => {
                 path='/edit-profile'
                 component={EditProfile}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
